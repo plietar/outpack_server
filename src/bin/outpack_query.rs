2d720b3c473b04117a0ebf0029529ec04fdc3ddb
Rob Ashton <r.ashton@imperial.ac.uk>
2023-04-12 10:07:53 +0100

    Simplify arg parsing as we know options must have a value

--- src/bin/outpack_query.rs
+++ src/bin/outpack_query.rs
@@ -8,31 +8,27 @@ fn print_usage(program: &str, opts: Options) {
     print!("{}", opts.usage(&brief));
 }
 
-fn parse_args(args: &[String]) -> (Option<String>, Option<String>) {
+fn parse_args(args: &[String]) -> (String, String) {
     let program = args[0].clone();
     let mut opts = Options::new();
     opts.reqopt("q", "query", "outpack query (required)", "latest");
     opts.reqopt("r", "root", "outpack root path (required)", ".");
     let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
+        Ok(m) => m,
         Err(f) => {
             print_usage(&program, opts);
             panic!("{}", f.to_string())
         }
     };
-    // TODO: Why do we return some here when we know there is a value? Especially after unwrapping
-    (Some(matches.opt_str("r").unwrap()), Some(matches.opt_str("q").unwrap()))
+    (matches.opt_str("r").unwrap(), matches.opt_str("q").unwrap())
 }
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
     let (root, query) = parse_args(&args);
-    if root.is_some() {
-        let root_path = root.unwrap();
-        let result = outpack::query::run_query(&root_path, query.unwrap());
-        match result {
-            Ok(res) => println!("{}", res),
-            Err(e) => println!("{}", e)
-        }
+    let result = outpack::query::run_query(&root, query);
+    match result {
+        Ok(res) => println!("{}", res),
+        Err(e) => println!("{}", e),
     }
 }
