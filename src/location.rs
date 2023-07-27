4e16be6e5bbc0154694392b1db8ffe9819cc8242
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-07-27 15:05:46 +0100

    Try new approach

--- src/location.rs
+++ src/location.rs
@@ -1,9 +1,11 @@
 use serde::{Deserialize, Serialize};
 use std::{fs, io};
+use std::ffi::{OsString};
 use std::fs::{DirEntry};
 use std::path::{Path, PathBuf};
 use cached::cached_result;
 use cached::instant::SystemTime;
+use crate::config::Location;
 use crate::utils::time_as_num;
 
 use super::config;
@@ -26,6 +28,14 @@ cached_result! {
     }
 }
 
+fn get_order(location_config: &[Location], entry: &DirEntry) -> usize {
+    let id = entry.file_name();
+    location_config.iter()
+        .position(|l| OsString::from(&l.id) == id)
+        .unwrap()
+}
+
+
 pub fn read_location(path: PathBuf) -> io::Result<Vec<LocationEntry>> {
     let mut packets = fs::read_dir(path)?
         .filter_map(|e| e.ok())
@@ -43,11 +53,15 @@ pub fn read_locations(root_path: &str) -> io::Result<Vec<LocationEntry>> {
         .join(".outpack")
         .join("location");
 
-    let locations = fs::read_dir(path)?
+    let location_config = config::read_config(root_path)?.location;
+
+    let mut locations_sorted = fs::read_dir(path)?
         .filter_map(|r| r.ok())
         .collect::<Vec<DirEntry>>();
 
-    let packets = locations
+    locations_sorted.sort_by_key(|a| get_order(&location_config, a));
+
+    let packets = locations_sorted
         .iter()
         .map(|entry| read_location(entry.path()))
         // collect any errors at this point into a single result
@@ -98,6 +112,15 @@ mod tests {
     use std::time::{Duration, SystemTime};
     use crate::test_utils::tests::get_temp_outpack_root;
     use super::*;
+
+    #[test]
+    fn packets_ordered_by_location_priority_then_id() {
+        let entries = read_locations("tests/example").unwrap();
+        assert_eq!(entries[0].packet, "20170818-164847-7574883b");
+        assert_eq!(entries[1].packet, "20170818-164830-33e0ab01");
+        assert_eq!(entries[2].packet, "20180220-095832-16a4bbed");
+        assert_eq!(entries[3].packet, "20180818-164043-7cdcde4b");
+    }
 
     #[test]
     fn can_find_local_id() {
