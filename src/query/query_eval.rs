fb357217a7784dbd692a451eb1fcb96f052b6e24
Rob Ashton <r.ashton@imperial.ac.uk>
2023-08-18 10:02:42 +0100

    Add some docs to parameter equals function

--- src/query/query_eval.rs
+++ src/query/query_eval.rs
@@ -53,6 +53,19 @@ fn eval_lookup<'a>(
 }
 
 impl Packet {
+    /// Check if a packet parameter is equal to a test value
+    ///
+    /// This will get the parameter `param_name` from the current Packet and then test for
+    /// equality of the test value `value`.
+    ///
+    /// # Arguments
+    /// * `param_name` - A string slice that holds the name of the parameter to test
+    /// * `value` - A LookupRhs which holds the value to check equality for, can be a boolean, a
+    ///             string, an integer or a float
+    ///
+    /// # Return
+    /// * bool - true if the current packet has a parameter called `param_name` and its value
+    ///          is equal to the LookupRhs.
     fn parameter_equals(&self, param_name: &str, value: &LookupRhs) -> bool {
         if let Some(json_value) = self.get_parameter(param_name) {
             match (json_value, value) {
